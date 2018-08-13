8a331e8affcbb3aee317defacfe3690f82715b2c
Armando Soriano <asoriano.dev@gmail.com>
2018-08-13 08:53:22 +0200

    feat(weather-controller): set weather controllers for bulk and single city request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { WeatherController } from './controllers/weather/weather.controller';
+import {
+  WeatherController,
+  WeatherGroupController,
+} from './controllers/weather/weather.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, WeatherController],
+  controllers: [AppController, WeatherController, WeatherGroupController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/controllers/weather/weather.controller.ts
+++ src/controllers/weather/weather.controller.ts
@@ -1,14 +1,21 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { CITIES_DATA } from './weather.mock';
 
-@Controller('group')
+@Controller('weather')
 export class WeatherController {
+  @Get()
+  getData(@Query() query: any) {
+    return CITIES_DATA.find(city => city.id.toString() === query.id);
+  }
+}
+
+@Controller('group')
+export class WeatherGroupController {
   @Get()
   getData(@Query() query: any) {
     return {
       cnt: 4,
       list: CITIES_DATA,
     };
-    // return CITIES_DATA.find(city => city.id.toString() === query.id);
   }
 }
